a012f94adc6ab871ecaf12e43c46cb2973581fa6
Dev <ydev6326@gmail.com>
2026-05-09 00:14:53 +0530

    Add customer clustering prediction pipeline

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.clustering import run_kmeans, run_dbscan
 from src.visualize import plot_cluster
 from src.tuning import Elbow_Method,find_eps
 from src.analysis import analysis_clusters
+import joblib
 # load data
 df = load_data("data/Mall_Customers.csv")
 
@@ -15,7 +16,7 @@ scaled_x, scaler = scale_data(X)
 Elbow_Method(scaled_x)
 find_eps(scaled_x)
 # clustering
-kmeans_labels, _ = run_kmeans(scaled_x)
+kmeans_labels, kmeans_model = run_kmeans(scaled_x)
 dbscan_labels, _ = run_dbscan(scaled_x)
 
 
@@ -28,3 +29,25 @@ analysis_clusters(df)
 # visulization 
 plot_cluster(df, "kmeans_cluster")
 plot_cluster(df, "dbscan_cluster")
+
+import pandas as pd
+new_customer =pd.DataFrame([[28, 85, 82]],columns=[
+        "Age",
+        "Annual Income (k$)",
+        "Spending Score (1-100)"
+    ]
+)
+new_customer_scaled=scaler.transform(new_customer)
+predicted_cluster = kmeans_model.predict(new_customer_scaled)
+print("predicted cluster:",predicted_cluster[0])
+
+
+joblib.dump(
+    kmeans_model,"model/kmeans_model.pkl"
+)
+
+joblib.dump(
+    scaler,"model/scaler.pkl"
+)
+
+print("Model saved successfully")
